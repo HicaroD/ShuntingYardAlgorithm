d66a46c9bf21ee1ddbbdb9d108a6f10c116d686b
HicaroD <hdanrlley1@gmail.com>
2022-07-03 17:13:02 -0300

    fmt(sy): applying `black` code formatter

--- sy/main.py
+++ sy/main.py
@@ -1,22 +1,9 @@
+from lexer import Lexer, Token
+
+
 class ShuntingYard:
-    def __init__(self, expression: str):
-        self.expression = expression
-        self.cursor = 0
-        self.current_token = None
-
-        if not self.is_expression_empty():
-            self.current_token = self.expression[0]
-
-    def is_expression_empty(self):
-        return not self.expression or self.expression.isspace()
-
-    def is_end_of_expression(self):
-        return self.cursor >= len(self.expression)
-
-    def advance(self):
-        if not self.is_end_of_expression():
-            self.current_token = self.expression[self.cursor]
-            self.cursor += 1
+    def __init__(self, tokens: list[Token]):
+        self.tokens = tokens
 
     def get_precedence(self, token) -> int:
         match token:
@@ -32,45 +19,26 @@ class ShuntingYard:
     def has_higher_precedence(self, top, current):
         return self.get_precedence(top) > self.get_precedence(current)
 
-    def is_operator(self, token):
-        return token in {"+", "-", "/", "*"}
-
     def get_rpn(self):
         output = []
         stack = []
 
-        if self.current_token != None:
-            while not self.is_end_of_expression():
-                print(self.current_token)
-
-                if self.current_token.isdigit():
-                    output.append(self.current_token)
-                    self.advance()
-
-                elif self.is_operator(self.current_token):
-                    self.advance()
-
-                elif self.current_token == "(":
-                    stack.append(self.current_token)
-                    self.advance()
-
-                elif self.current_token == ")":
-                    self.advance()
-
-                else:
-                    raise NotImplementedError(f"Invalid token: {self.current_token}")
-        else:
-            raise ValueError("Empty expression")
+        for token in self.tokens:
+            # TODO: Implement shunting yard algorithm
+            print(token)
 
         return output
 
 
 def main():
-    expressions = ["2+3", "5+3+4", "5*3+4", "8/4/2^"]
+    expressions = ["2+3", "5+3+4", "5*3+4", "8/4/2"]
 
     for expression in expressions:
         try:
-            shunting_yard = ShuntingYard(expression)
+            lexer = Lexer(expression)
+            tokens = lexer.tokenize()
+
+            shunting_yard = ShuntingYard(tokens)
             rpn_expression = shunting_yard.get_rpn()
             print(f"{expression} = {rpn_expression}")
         except (ValueError, NotImplementedError) as e:
